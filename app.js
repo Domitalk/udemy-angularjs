87b55714deca5d5cd97cc8eb40f087949bbbcd21
Dominic Chu <dominic.m.chu@gmail.com>
2022-03-09 16:15:43 -0500

    basics of angularjs directives

--- app.js
+++ app.js
@@ -1,11 +1,62 @@
+var myApp = angular.module('myApp', [])
+
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
+    $scope.handle = ''
+    $scope.lowercasehandle = function() {
+        return $filter('lowercase')($scope.handle)
+    }
+    $scope.characters = 5
+    
+}])
+
+
+
+// var myApp = angular.module('myApp', [])
+
+// // scope is what ties the virtualDOM to changes in values 
+// myApp.controller('mainController', ['$scope', '$timeout', '$filter', function($scope, $timeout, $filter) {
+//     // this .handle will be now tied to the html
+//     $scope.handle = ''
+//     $scope.lowercasehandle = function() {
+//         return $filter('lowercase')($scope.handle)
+//     }
+//     $scope.name = 'Tony'
+//     // you will see the name change in the webpage after 3 secs
+//     $timeout(function() {
+//         $scope.name = 'Everybody'
+//     }, 3000)
+
+//     setTimeout(function() {
+//         // have to wrap this in $apply for it actually change the dom
+//         // setTimeout or anything NOT angular will thread out a new process that angularjs isn't watching 
+//         // the easy hack for this is obviously using $timeout instead of setTimeout()
+//         $scope.$apply(function() {
+//             $scope.handle = 'newtwitterhandle'
+//             console.log('Scope changed!')
+//         })
+//     }, 3000)
+    
+// }])
+
+
+// basically angularJS is a giant net of eventloops all tied to variables being passed between the JS and HTML
+// anytime something changes, that might have an effect on the VDOM, it'll probably have a eventloop tied to it 
+// a lot of addEventListeners are basically written out for you as long as all the proper steps are taken
+// this is different from REACT and newer frameworks in that the newer ones go one step beyond that
+// and they try to combine the two files HTML and JS into one. and represent the HTML as a declarative JSX
+// AngularJS is a inbetween step wherein a lot of jquery is done for you
+// but the visual representation of the written code isnt' yet unified 
+
+
 // match name with index ng
 // the array holds dependency modules 
-var myApp = angular.module('myApp', ['ngMessages'])
+// var myApp = angular.module('myApp', ['ngMessages', 'ngResource'])
 // had to add in the script tag for ngMessages? 
+
 
 // match name with index ng
 // these args $scope, $log etc are dependency injections ALSO
-myApp.controller('mainController', function($scope, $log, $filter) {
+// myApp.controller('mainController', function($scope, $log, $filter, $resource) {
 
     // $scope.name = 'Jane Doe'
     // $scope.occupation = 'Coder'
@@ -31,7 +82,10 @@ myApp.controller('mainController', function($scope, $log, $filter) {
     // $log.info($scope.formattedname)
     // // 'JOHN'
 
-})
+
+    // console.log($resource)
+
+// })
 
 
 
