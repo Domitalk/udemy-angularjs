75569340a008466cd81e6245739c04e3abb7741b
Dominic Chu <dominic.m.chu@gmail.com>
2022-03-09 13:09:48 -0500

    update imports on html doc

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+// match name with index ng
 var myApp = angular.module('myApp', [])
+
+// match name with index ng
+myApp.controller('mainController', function($scope) {
+
+    $scope.name = 'Jane Doe'
+    $scope.occupation = 'Coder'
+
+    $scope.getname = function() {
+        return 'John Doe'
+    }
+
+    $scope.getname()
+    console.log($scope)
+
+})
+
+
+
+// NOTES 
+// angularjs uses dependency injection. 
+// basically using variables in memory to change up the html
